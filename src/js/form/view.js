16af533365a6a2ad88e31db03cb706ba2b577a7a
AINER <den.djomin@mail.ru>
2024-09-25 23:33:18 +0300

    REFACTORING: view.js render function

--- src/js/form/view.js
+++ src/js/form/view.js
@@ -3,27 +3,23 @@
 import _ from 'lodash';
 
 export default (elements) => (path, value) => {
-  console.log(value);
-  if (value.includes('duplicated')) {
-    elements.input.classList.add('is-invalid');
-    elements.errorMessage.textContent = 'Эта ссылка уже добавлена';
+  if (!_.isEmpty(value)) {
     elements.submit.classList.add('disabled');
-
-    return;
-  }
-
-  if (value.includes('is-not-a-link')) {
     elements.input.classList.add('is-invalid');
-    elements.errorMessage.textContent = 'Некорректная ссылка';
-    elements.submit.classList.add('disabled');
+
+    if (value.includes('duplicated')) {
+      elements.errorMessage.textContent = 'Эта ссылка уже добавлена';
+    } else if (value.includes('is-not-a-link')) {
+      elements.errorMessage.textContent = 'Некорректная ссылка';
+    }
 
     return;
   }
 
   if (_.isEmpty(value)) {
+    elements.submit.classList.remove('disabled');
     elements.input.classList.remove('is-invalid');
     elements.errorMessage.textContent = '';
-    elements.submit.classList.remove('disabled');
 
     return;
   }
