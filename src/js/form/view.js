93ac9be3fd64cf76d9e4267476b7c3b8c517419e
AINER <den.djomin@mail.ru>
2024-09-26 13:52:03 +0300

    FIX: Incorrect render function blocking entering of second value

--- src/js/form/view.js
+++ src/js/form/view.js
@@ -3,17 +3,19 @@
 import _ from 'lodash';
 
 export default (elements) => (path, value) => {
-  if (!_.isEmpty(value)) {
-    elements.submit.classList.add('disabled');
+  if (value.includes('duplicated') || value.includes('is-not-a-link')) {
     elements.input.classList.add('is-invalid');
+    elements.submit.classList.add('disabled');
 
     if (value.includes('duplicated')) {
       elements.errorMessage.textContent = 'Эта ссылка уже добавлена';
-    } else if (value.includes('is-not-a-link')) {
-      elements.errorMessage.textContent = 'Некорректная ссылка';
+      return;
     }
 
-    return;
+    if (value.includes('is-not-a-link')) {
+      elements.errorMessage.textContent = 'Некорректная ссылка';
+      return;
+    }
   }
 
   if (_.isEmpty(value)) {
