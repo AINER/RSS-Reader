b9a8d749554e046ad4a855c744ebd0f18cd87fa3
AINER <den.djomin@mail.ru>
2024-09-29 00:47:28 +0300

    Change decorative image name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.scss';
 import 'bootstrap';
 
 // Images
-import bongoСatImage from './assets/bongo-cat-typing.gif';
+import bongoСatImage from './assets/bongo_cat_typing.gif';
 import iconImage from './assets/icon.png';
 
 import formApp from './js/form/main';
