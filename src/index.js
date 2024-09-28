53114dc56e7725df1a4a6d15ce958ada204373b6
AINER <den.djomin@mail.ru>
2024-09-29 00:52:47 +0300

    Add import and initialization for localization

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import 'bootstrap';
 import bongoСatImage from './assets/bongo_cat_typing.gif';
 import iconImage from './assets/icon.png';
 
+import initI18next from './js/localization/init_localization';
+import languageSwitch from './js/localization/language_switch';
 import formApp from './js/form/main';
 
+initI18next();
+languageSwitch();
 formApp();
