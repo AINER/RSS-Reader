e8b57c806440f7dcf09c7ebbd3a1769fa445f77f
AINER <den.djomin@mail.ru>
2024-11-18 16:56:22 +0300

    Delete WebpackBundleAnalyzer

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -21,7 +20,6 @@ const config = {
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
-    new BundleAnalyzerPlugin(),
 
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
