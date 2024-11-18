298769ac0e601a38554d8d38e32d2f7080df3a74
AINER <den.djomin@mail.ru>
2024-11-18 14:18:13 +0300

    Install Webpack bundle analyzer as dev dependency

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -20,6 +21,7 @@ const config = {
     new HtmlWebpackPlugin({
       template: "index.html",
     }),
+    new BundleAnalyzerPlugin(),
 
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
