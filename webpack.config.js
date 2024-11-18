5101bedcec1cca4707152e891913bf872a3a627f
AINER <den.djomin@mail.ru>
2024-11-18 14:19:57 +0300

    Change Webpack bundle directory from dist to public

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const stylesHandler = "style-loader";
 const config = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
   },
   devServer: {
     open: true,
