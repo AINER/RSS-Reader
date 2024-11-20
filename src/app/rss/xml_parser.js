642535bb68aaf28ec92c8ed3e519afc672dee2ce
AINER <den.djomin@mail.ru>
2024-11-20 23:51:22 +0300

    FIX: mapping of parsed data
    Added check for existence of data element and logic for its selection
    to form the resulting post object.

--- src/app/rss/xml_parser.js
+++ src/app/rss/xml_parser.js
@@ -5,17 +5,32 @@ export default (xmlString, channelUrl) => {
   const xmlDocument = parser.parseFromString(xmlString, 'application/xml');
 
   const items = Array.from(xmlDocument.querySelectorAll('item'));
-  const posts = items.map((item) => (
-    {
-      title: item.querySelector('title').textContent,
-      description: item.querySelector('description').textContent,
-      publicationDate: item.querySelector('pubDate').textContent,
-      originalPostLink: item.querySelector('link').textContent,
-      creator: item.querySelector('creator').textContent,
+
+  const posts = items.map((item) => {
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const publicationDate = item.querySelector('pubDate').textContent;
+    const originalPostLink = item.querySelector('link').textContent;
+    let creator;
+    if (item.querySelector('author') !== null) {
+      creator = item.querySelector('author').textContent;
+    } else if (item.querySelector('creator') !== null) {
+      creator = item.querySelector('creator').textContent;
+    } else {
+      creator = '';
+    }
+
+    return {
+      title,
+      description,
+      publicationDate,
+      originalPostLink,
+      creator,
 
       id: uuidv4(),
       read: false,
-    }));
+    };
+  });
 
   const result = {
     title: xmlDocument.querySelector('title').textContent,
