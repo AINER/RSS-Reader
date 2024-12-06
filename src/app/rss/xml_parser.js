5db9732e4f09f49641656ab74e97940d8997e8d0
AINER <den.djomin@mail.ru>
2024-12-06 19:36:58 +0300

    Change parsing and rendering for post view pop-up
    Changed to be more flexible and avoid bugs with some feeds.

--- src/app/rss/xml_parser.js
+++ src/app/rss/xml_parser.js
@@ -4,12 +4,33 @@ export default (xmlString, channelUrl) => {
   const parser = new DOMParser();
   const xmlDocument = parser.parseFromString(xmlString, 'application/xml');
 
-  const items = Array.from(xmlDocument.querySelectorAll('item'));
+  // --- Posts parsing ---
+
+  let items;
+  if (xmlDocument.querySelectorAll('item') !== null && xmlDocument.querySelectorAll('item').length !== 0) {
+    items = Array.from(xmlDocument.querySelectorAll('item'));
+  } else if (xmlDocument.querySelectorAll('entry') !== null) {
+    items = Array.from(xmlDocument.querySelectorAll('entry'));
+  }
 
   const posts = items.map((item) => {
     const title = item.querySelector('title').textContent;
-    const description = item.querySelector('description').textContent;
-    const publicationDate = item.querySelector('pubDate').textContent;
+    let description;
+    if (item.querySelector('description') !== null) {
+      description = item.querySelector('description').textContent;
+    } else if (item.querySelector('content') !== null) {
+      description = item.querySelector('content').textContent;
+    } else {
+      throw new Error('Author not parsed');
+    }
+    let publicationDate;
+    if (item.querySelector('pubDate') !== null) {
+      publicationDate = item.querySelector('pubDate').textContent;
+    } else if (item.querySelector('published') !== null) {
+      publicationDate = item.querySelector('published').textContent;
+    } else {
+      throw new Error('Publication date not parsed');
+    }
     const originalPostLink = item.querySelector('link').textContent;
     let creator;
     if (item.querySelector('author') !== null) {
@@ -17,7 +38,7 @@ export default (xmlString, channelUrl) => {
     } else if (item.querySelector('creator') !== null) {
       creator = item.querySelector('creator').textContent;
     } else {
-      creator = '';
+      throw new Error('Author not parsed');
     }
 
     return {
@@ -32,9 +53,18 @@ export default (xmlString, channelUrl) => {
     };
   });
 
+  // --- Feed parsing ---
+  let description;
+
+  if (xmlDocument.querySelector('description') !== null) {
+    description = xmlDocument.querySelector('description').textContent;
+  } else {
+    description = '';
+  }
+
   const result = {
     title: xmlDocument.querySelector('title').textContent,
-    description: xmlDocument.querySelector('description').textContent,
+    description,
     url: channelUrl,
     posts,
   };
